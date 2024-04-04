8f021a89842a2d990df94a8cc9cb18e86a35dadd
Matthew Pratt <matthew.pratt13@gmail.com>
2024-04-04 10:07:28 +0200

    add comments

--- src/lexer.rs
+++ src/lexer.rs
@@ -8,6 +8,7 @@ use crate::{
     U256,
 };
 
+/// Lexer struct, containing methods to render tokens (tokenize) from characters in a source string.
 struct Lexer<'a> {
     input: &'a str,
     pos: usize,
@@ -35,7 +36,7 @@ impl<'a> Lexer<'a> {
     fn tokenize_doc_line_comment(&mut self) -> Token {
         let start_pos = self.pos;
 
-        self.pos += 3; // Skip the "///"
+        self.pos += 3; // skip doc comment prefix (`///`)
 
         while self.pos < self.input.len() {
             if self.input.chars().nth(self.pos).unwrap() == '\n' {
@@ -53,7 +54,7 @@ impl<'a> Lexer<'a> {
     fn tokenize_doc_block_comment(&mut self) -> Token {
         let start_pos = self.pos;
 
-        self.pos += 3; // Skip the "/**" delimiter
+        self.pos += 3; // skip doc block comment prefix (`/**`)
 
         while self.pos + 1 < self.input.len() && &self.input[self.pos..self.pos + 2] != "*/" {
             self.pos += 1;
@@ -62,7 +63,7 @@ impl<'a> Lexer<'a> {
         let comment = self.input[start_pos..self.pos].trim().to_string();
         let span = Span::new(self.input, start_pos, self.pos);
 
-        self.pos += 2; // Skip the "*/" delimiter
+        self.pos += 2; // skip comment terminator (`*/`)
 
         Token::DocComment { comment, span }
     }
@@ -108,19 +109,19 @@ impl<'a> Lexer<'a> {
 
         match delim {
             '(' => {
-                self.pos += 1; // Move past the opening delimiter
+                self.pos += 1; // skip opening delimiter
                 let span = Span::new(self.input, start_pos, self.pos);
                 Ok(Token::LParen { delim, span })
             }
 
             '{' => {
-                self.pos += 1; // Move past the opening delimiter
+                self.pos += 1; //  skip opening delimiter
                 let span = Span::new(self.input, start_pos, self.pos);
                 Ok(Token::LBrace { delim, span })
             }
 
             '[' => {
-                self.pos += 1; // Move past the opening delimiter
+                self.pos += 1; // skip opening delimiter
                 let span = Span::new(self.input, start_pos, self.pos);
                 Ok(Token::LBracket { delim, span })
             }
@@ -144,23 +145,23 @@ impl<'a> Lexer<'a> {
         let delim = self.input.chars().nth(self.pos).unwrap();
 
         if delim == expected {
-            self.pos += 1; // Move past the closing delimiter
+            self.pos += 1; // skip closing delimiter
 
             match delim {
                 ')' => {
-                    self.pos += 1; // Move past the opening delimiter
+                    self.pos += 1; // skip opening delimiter
                     let span = Span::new(self.input, start_pos, self.pos);
                     Ok(Token::RParen { delim, span })
                 }
 
                 '}' => {
-                    self.pos += 1; // Move past the opening delimiter
+                    self.pos += 1; // skip opening delimiter
                     let span = Span::new(self.input, start_pos, self.pos);
                     Ok(Token::RBrace { delim, span })
                 }
 
                 ']' => {
-                    self.pos += 1; // Move past the opening delimiter
+                    self.pos += 1; // skip opening delimiter
                     let span = Span::new(self.input, start_pos, self.pos);
                     Ok(Token::RBracket { delim, span })
                 }
@@ -177,6 +178,63 @@ impl<'a> Lexer<'a> {
                 error_kind: LexerErrorKind::MismatchedDelimiter(delim),
                 pos: self.pos,
             });
+        }
+    }
+
+    fn tokenize_string(&mut self) -> Result<Token, LexerErrorKind> {
+        let mut value = String::new();
+
+        let start_pos = self.pos;
+
+        self.pos += 1; // skip opening quote
+
+        while self.pos < self.input.len() {
+            let current_char = self.input.chars().nth(self.pos).unwrap();
+            match current_char {
+                '\\' => {
+                    // handle escape sequences
+                    if let Some(escaped_char) = self.parse_escape_sequence() {
+                        value.push(escaped_char);
+                    } else {
+                        return Err(LexerErrorKind::InvalidEscapeSequenceInStringLiteral);
+                    }
+                }
+                '"' => {
+                    self.pos += 1; // skip closing quote
+
+                    let span = Span::new(self.input, start_pos, self.pos);
+
+                    return Ok(Token::StringLiteral { value, span });
+                }
+                _ => {
+                    value.push(current_char);
+                    self.pos += 1;
+                }
+            }
+        }
+
+        Err(LexerErrorKind::UnclosedStringLiteral)
+    }
+
+    fn tokenize_char(&mut self) -> Token {
+        todo!()
+    }
+
+    fn tokenize_u256(&mut self) -> Token {
+        let start_pos = self.pos;
+
+        while self.pos < self.input.len()
+            && self.input[self.pos..].chars().next().unwrap().is_numeric()
+        {
+            self.pos += 1;
+        }
+
+        let number = self.input[start_pos..self.pos].parse::<U256>();
+        let span = Span::new(self.input, start_pos, self.pos);
+
+        match number {
+            Ok(value) => Token::U256Literal { value, span },
+            Err(_) => todo!(),
         }
     }
 
@@ -203,65 +261,9 @@ impl<'a> Lexer<'a> {
         }
     }
 
-    fn tokenize_u256(&mut self) -> Token {
-        let start_pos = self.pos;
-
-        while self.pos < self.input.len()
-            && self.input[self.pos..].chars().next().unwrap().is_numeric()
-        {
-            self.pos += 1;
-        }
-
-        let number = self.input[start_pos..self.pos].parse::<U256>();
-        let span = Span::new(self.input, start_pos, self.pos);
-
-        match number {
-            Ok(value) => Token::U256Literal { value, span },
-            Err(_) => todo!(),
-        }
-    }
-
-    fn tokenize_string(&mut self) -> Result<Token, LexerErrorKind> {
-        let mut value = String::new();
-
-        let start_pos = self.pos;
-
-        self.pos += 1; // Skip the opening quote
-
-        while self.pos < self.input.len() {
-            let current_char = self.input.chars().nth(self.pos).unwrap();
-            match current_char {
-                '\\' => {
-                    // Handle escape sequences
-                    if let Some(escaped_char) = self.parse_escape_sequence() {
-                        value.push(escaped_char);
-                    } else {
-                        return Err(LexerErrorKind::InvalidEscapeSequenceInStringLiteral);
-                    }
-                }
-                '"' => {
-                    self.pos += 1; // Skip the closing quote
-
-                    let span = Span::new(self.input, start_pos, self.pos);
-
-                    return Ok(Token::StringLiteral { value, span });
-                }
-                _ => {
-                    value.push(current_char);
-                    self.pos += 1;
-                }
-            }
-        }
-
-        Err(LexerErrorKind::UnclosedStringLiteral)
-    }
-
-    fn tokenize_char(&mut self) -> Token {
-        todo!()
-    }
-
+    /// Parse an escape sequence found in a string or character literal.
     fn parse_escape_sequence(&mut self) -> Option<char> {
-        self.pos += 1; // Skip the backslash
+        self.pos += 1; // skip backslash
 
         if self.pos < self.input.len() {
             let escaped_char = match self.input.chars().nth(self.pos).unwrap() {
@@ -270,21 +272,22 @@ impl<'a> Lexer<'a> {
                 '\\' => '\\',
                 '"' => '"',
                 // TODO: handle other escape sequences
-                _ => return None, // Invalid escape sequence
+                _ => return None, // invalid escape sequence
             };
             self.pos += 1;
             Some(escaped_char)
         } else {
-            None // Incomplete escape sequence
+            None // incomplete escape sequence
         }
     }
 
-    
+    /// Check if some string is a reserved keyword (as opposed to an ordinary identifier).
     fn is_keyword(&self, value: &str) -> bool {
         let keywords = vec!["let", "func", "if", "else", "while", "for"]; // TODO: add keywords
         keywords.contains(&value)
     }
 
+    /// Skip the source string's whitespace, which is considered unnecessary in tokenization.
     fn skip_whitespace(&mut self) {
         while self.pos < self.input.len()
             && self.input.chars().nth(self.pos).unwrap().is_whitespace()
