83fd733cbc5603013953cce433dd0de585e695e1
Matthew Pratt <matthew.pratt13@gmail.com>
2024-04-05 23:23:07 +0200

    add fixed-hash crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,19 @@
 use uint::construct_uint;
+use fixed_hash::{construct_fixed_hash, impl_fixed_hash_conversions};
 
 construct_uint! {
     pub struct U256(4);
 }
+
+construct_fixed_hash! {
+    pub struct H256(32);
+}
+
+construct_fixed_hash! {
+    pub struct H160(20);
+}
+
+impl_fixed_hash_conversions!(H256, H160);
 
 mod ast;
 mod error;
