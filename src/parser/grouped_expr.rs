406d8371b1f0172b4fbb129f0f6f865129410cf7
Matthew Pratt <matthew.pratt13@gmail.com>
2024-04-14 15:54:05 +0200

    add grouped_expr.rs

---
+++ src/parser/grouped_expr.rs
@@ -0,0 +1,48 @@
+use crate::{
+    ast::{Delimiter, GroupedExpr},
+    error::ErrorsEmitted,
+    token::Token,
+};
+
+use super::{Parser, Precedence};
+
+impl GroupedExpr {
+    pub(crate) fn parse(parser: &mut Parser) -> Result<GroupedExpr, ErrorsEmitted> {
+        let expression = parser.parse_expression(Precedence::Lowest)?;
+
+        let close_paren = parser.expect_delimiter(Token::RParen {
+            delim: ')',
+            span: parser.stream.span(),
+        });
+
+        if !parser.errors().is_empty() {
+            return Err(ErrorsEmitted(()));
+        }
+
+        Ok(GroupedExpr {
+            open_paren: Delimiter::LParen,
+            expression: Box::new(expression),
+            close_paren: close_paren?,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::test_utils;
+
+    #[test]
+    fn test_grouped_expr() -> Result<(), ()> {
+        let input = r#"(x + 2)"#;
+
+        let mut parser = test_utils::get_parser(input);
+
+        let expressions = parser.parse();
+
+        match expressions {
+            Ok(t) => Ok(println!("{:#?}", t)),
+            Err(_) => Err(println!("{:#?}", parser.errors())),
+        }
+    }
+}
