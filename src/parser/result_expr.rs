7dcd76a92f8c9363628b858fe161875cbf1273c9
Matthew Pratt <matthew.pratt13@gmail.com>
2024-04-28 11:21:28 +0200

    fix ResultExpr

--- src/parser/result_expr.rs
+++ src/parser/result_expr.rs
@@ -28,6 +28,8 @@ impl ResultExpr {
 
         let expression = parser.parse_expression(Precedence::Lowest)?;
 
+        parser.consume_token();
+
         parser.expect_delimiter(TokenType::RParen)?;
 
         let expr = ResultExpr {
