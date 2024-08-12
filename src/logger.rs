25d17679b289ed857c664543f21829aca9fedc9c
Matthew Pratt <matthew.pratt13@gmail.com>
2024-08-12 14:29:32 +0200

    update logging / error reporting formatting

--- src/logger.rs
+++ src/logger.rs
@@ -42,7 +42,7 @@ impl Logger {
         if self.level <= msg.level {
             self.messages.push(msg);
         } else {
-            self.messages.push(LogMsg { level: msg.level, text: format!("tried to log message and failed. Verbosity level ({:?}) is lower than the initialized one({:?})", msg.level, self.level)});
+            self.messages.push(LogMsg { level: msg.level, text: format!("tried to log message and failed. Verbosity level ({}) is lower than the initialized one ({})", msg.level, self.level)});
         }
     }
 
@@ -76,8 +76,12 @@ impl Logger {
 
     /// Retrieve the log messages.
     #[allow(dead_code)]
-    pub fn messages(&self) -> &[LogMsg] {
-        &self.messages
+    pub fn messages(&self) -> Vec<String> {
+        self.messages
+            .iter()
+            .map(|m| m.to_string())
+            .collect::<Vec<_>>()
+            .clone()
     }
 
     /// Clear all log messages.
